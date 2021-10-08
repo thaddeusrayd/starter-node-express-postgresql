1f8f4ae8b3c6f026b601471744ee054f029352ca
thaddeusrayd <thaddeusrayd@gmail.com>
2021-10-08 10:29:22 -0500

    correct products_categories code

--- src/db/migrations/20211008132742_createProductsCategoriesTable.js
+++ src/db/migrations/20211008132742_createProductsCategoriesTable.js
@@ -1,7 +1,20 @@
 exports.up = function (knex) {
   return knex.schema.createTable("products_categories", (table) => {
-    table.increments("category_id").primary();
-    table.increments("product_id");
+    table.integer("product_id").unsigned().notNullable();
+    table
+      .foreign("product_id")
+      .references("product_id")
+      .inTable("products")
+      .onDelete("CASCADE");
+
+    table.integer("category_id").unsigned().notNullable();
+    table
+      .foreign("category_id")
+      .references("category_id")
+      .inTable("categories")
+      .onDelete("CASCADE");
+
+    table.timestamps(true, true);
   });
 };
 
