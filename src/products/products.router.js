ec8a100b1cfab149ee1d02c68d5569be55df716a
thaddeusrayd <thaddeusrayd@gmail.com>
2021-10-12 09:49:12 -0500

    add route for out of stock

--- src/products/products.router.js
+++ src/products/products.router.js
@@ -4,5 +4,9 @@ const methodNotAllowed = require("../errors/methodNotAllowed");
 
 router.route("/").get(controller.list).all(methodNotAllowed);
 router.route("/:productId([0-9]+)").get(controller.read).all(methodNotAllowed);
+router
+  .route("/out-of-stock-count")
+  .get(controller.listOutOfStockCount)
+  .all(methodNotAllowed);
 
 module.exports = router;
